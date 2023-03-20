7aba889623754c309885c30328281f232eacfe48
rvkoon <koontchak@gmail.com>
2023-03-20 17:13:21 +0100

    fix wrong api url

--- dist/constants.js
+++ dist/constants.js
@@ -2,5 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.API_URL = exports.WEB3AUTH_CLIENT_ID = void 0;
 exports.WEB3AUTH_CLIENT_ID = 'BH6A9sDxfEZQ96_dg9ret3NDZgwMjR-yArysjLff1oMCJQ3stc1J-yPXoUdf_zCzTt8A22D3McrFBy3eYKp4jnw';
-// export const API_URL = 'https://alembic-api.develop.cometh.tech'
-exports.API_URL = 'http://localhost:5000';
+exports.API_URL = 'https://alembic-api.develop.cometh.tech';
