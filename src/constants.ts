14f2e14909a1e6d30ff7fc94b96a69c685838b1e
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2023-04-19 16:45:25 +0200

    fix: const

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const WEB3AUTH_CLIENT_ID =
   'BH6A9sDxfEZQ96_dg9ret3NDZgwMjR-yArysjLff1oMCJQ3stc1J-yPXoUdf_zCzTt8A22D3McrFBy3eYKp4jnw'
-export const API_URL =
-  /* 'https://alembic-api.develop.cometh.tech' */ 'http://localhost:3000'
+export const API_URL = 'https://alembic-api.develop.cometh.tech'
 export const DEFAULT_CHAIN_ID = 137
 export const DEFAULT_RPC_TARGET = 'https://polygon-rpc.com'
 export const DEFAULT_BASE_GAS = 80000
