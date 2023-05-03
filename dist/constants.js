0e5e177c48a453816b4e1be77ee5e65e5064eba1
rvkoon <koontchak@gmail.com>
2023-05-03 13:49:59 +0200

    remove w3auth clientId

--- dist/constants.js
+++ dist/constants.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EIP712_SAFE_TX_TYPES = exports.EIP712_SAFE_MESSAGE_TYPE = exports.BLOCK_EVENT_GAP = exports.DEFAULT_REWARD_PERCENTILE = exports.DEFAULT_BASE_GAS = exports.networks = exports.DEFAULT_RPC_TARGET = exports.DEFAULT_CHAIN_ID = exports.API_URL = exports.WEB3AUTH_CLIENT_ID = void 0;
-exports.WEB3AUTH_CLIENT_ID = 'BH6A9sDxfEZQ96_dg9ret3NDZgwMjR-yArysjLff1oMCJQ3stc1J-yPXoUdf_zCzTt8A22D3McrFBy3eYKp4jnw';
+exports.EIP712_SAFE_TX_TYPES = exports.EIP712_SAFE_MESSAGE_TYPE = exports.BLOCK_EVENT_GAP = exports.DEFAULT_REWARD_PERCENTILE = exports.DEFAULT_BASE_GAS = exports.networks = exports.DEFAULT_RPC_TARGET = exports.DEFAULT_CHAIN_ID = exports.API_URL = void 0;
 exports.API_URL = 'https://alembic-api.develop.cometh.tech';
 exports.DEFAULT_CHAIN_ID = 137;
 exports.DEFAULT_RPC_TARGET = 'https://polygon-rpc.com';
