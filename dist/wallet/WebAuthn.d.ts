6fe6f2c9e93a1f53c999dd46ddf356cc16d907cf
rvkoon <72939897+rvkoon@users.noreply.github.com>
2023-05-15 11:23:28 +0200

    Delete dist directory

--- dist/wallet/WebAuthn.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-  createCredentials: (userId: string) => Promise<any>
-  getWebAuthnSignature: (
-    safeTxHash: string,
-    publicKey_Id: string
-  ) => Promise<string>
-}
-export default _default
