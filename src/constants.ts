8f215d15ae9eabfa8499bf182b0e33dba7c831be
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2023-08-07 15:28:16 +0200

    package to dev version

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://account-abstraction.api.alembic.tech/'
+export const API_URL = 'https://alembic-api.develop.cometh.tech/'
 export const DEFAULT_CHAIN_ID = 137
 export const DEFAULT_RPC_TARGET = 'https://polygon-rpc.com'
 export const networks = {
