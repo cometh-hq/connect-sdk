53bc27d70800b97ef51be7bf3f0c84c30c6b90b2
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2023-09-21 16:42:10 +0200

    update: production P256SignerCreationCode

--- src/services/webAuthnService.spec.ts
+++ src/services/webAuthnService.spec.ts
@@ -1,7 +1,7 @@
 import webAuthnService from './webAuthnService'
 
 const DEPLOYED_WEBAUTHN_SIGNER_ADDRESS =
-  '0x0C3a517aCE13BBCA055F8FFc5f97A1CFe732C381'
+  '0x5DfB2bf03a0Ad9d4870bE25Fb6c324048774D372'
 const CHAIN_ID = 137
 const publicKey_X =
   '0xaf0b3c3d191a70a11e2b0b0bc6216f3b960ad20dc0bb34920348fe7852e68d7a'
